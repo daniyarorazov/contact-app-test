b88f050d3b4cceb2e7b48be2a2286f54782cd2bf
Daniyar Orazov <orazov.video@gmail.com>
2023-11-22 16:48:26 +0100

    Edit message after company register

--- modules/company_register.js
+++ modules/company_register.js
@@ -43,7 +43,17 @@ function company_register(msg, bot) {
             case 6:
                 user.vacancy = msg.text;
                 const username = msg.from.username;
-                bot.sendMessage(chatId, `Новая анкета:\n\nИмя: ${user.name}\nФамилия: ${user.surname}\n Университет: ${user.university} \n Специальность: ${user.speciality}\nUsername: @${username}\n${userId}`);
+                bot.sendMessage(chatId, `Новая анкета:\n\n
+                    ID: ${dataToSave.id}\n
+                    Название компании: ${dataToSave.name_company}\n
+                    Тип компании: ${dataToSave.type_company}\n
+                    Веб-сайт компании: ${dataToSave.website_company}\n
+                    Имя-Фамилия-Роль сотрудника: ${dataToSave.name_surname_role_person}\n
+                    Готовность к оплате стажировки: ${dataToSave.ready_pay_intern}\n
+                    Вакансия: ${dataToSave.vacancy}\n
+                    Chat ID: ${dataToSave.chat_id}\n
+                    Username: @${username}\n${userId}
+                    `);
                 bot.sendMessage(chatId, 'Спасибо за заполнение анкеты!');
                 menu(msg, bot);
                 const config = {
